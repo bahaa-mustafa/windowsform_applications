d81bb7809a453b7f3b2970336c934f64bfd9d461
bahaa-mustafa <bahaamustafabadr@gmail.com>
2023-04-13 18:13:26 +0200

    handling some unexpected wrongs in code

--- simple calculator with advance UI/Form1.cs
+++ simple calculator with advance UI/Form1.cs
@@ -7,7 +7,7 @@ namespace simple_calculator_with_advance_UI
         double a1;
         double a2;
         double result;
-        string op_view;
+        string op_view = "";
         public Form1()
         {
             InitializeComponent();
@@ -18,31 +18,54 @@ namespace simple_calculator_with_advance_UI
         private void btn_click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            if (lb_result.Text == "")
+
+            // not accept code
+
+            /*  if (lb_result.Text == "")
+              {
+                 // lb_result.Text = btn.Text;
+                 // lb_calculator.Text =lb_calculator.Text + lb_result.Text;
+                  a1 = Convert.ToDouble(btn.Text);
+
+              }
+              else
+              {
+                 // lb_calculator.Text = lb_calculator.Text + lb_result.Text;
+                  a1 = Convert.ToDouble(lb_result.Text);
+                  lb_result.Text = "";
+                 // lb_result.Text = btn.Text;
+                  a2 = Convert.ToDouble(btn.Text);
+                 // lb_calculator.Text = lb_calculator.Text + lb_result.Text;
+              }
+            */
+
+            // anthor trying
+            
+            lb_result.Text = lb_result.Text + btn.Text;
+            if(op_view=="")
             {
-                lb_result.Text = btn.Text;
-                lb_calculator.Text =lb_calculator.Text + lb_result.Text;
-                a1 = Convert.ToDouble(btn.Text);
+                a1 = Convert.ToDouble(lb_result.Text);
 
             }
             else
             {
-                //lb_calculator.Text = lb_calculator.Text + lb_result.Text;
-                a1 = Convert.ToDouble(lb_result.Text);
-                lb_calculator.Text = lb_calculator.Text + lb_result.Text;
-                lb_result.Text = "";
-                lb_result.Text = btn.Text;
-                a2 = Convert.ToDouble(btn.Text);
-
+                
+                a2 = Convert.ToDouble(lb_result.Text);
+               // lb_calculator.Text = lb_calculator.Text + lb_result.Text;
             }
+
+
         }
         private void pro_click(object sender, EventArgs e)
         {
             Button btnOp = (Button)sender;
             op_view = btnOp.Text;
+
             //label1.Text = label1.Text + label2.Text;
-            lb_calculator.Text = lb_calculator.Text + " " + op_view + " ";
-            //lb_result.Text = "";
+           // a1 = Convert.ToDouble(lb_result.Text);
+            
+            lb_calculator.Text = lb_result.Text + " " + op_view + " ";
+            lb_result.Text = "";
         }
 
 
@@ -54,36 +77,39 @@ namespace simple_calculator_with_advance_UI
             {
                 case "+":
                     result = a1 + a2;
+                    op_view = "";
                     break;
                 case "-":
                     result = a1 - a2;
+                    op_view = "";
                     break;
                 case "/":
-                    result = a2 / a2;
+                    result = a1 / a2;
+                    op_view = "";
                     break;
 
                 case "%":
                     result = a1 % a2;
+                    op_view = "";
                     break;
 
                 default:
                     result = a1 * a2;
+                    op_view = "";
                     break;
             }
 
-            if (a1 >= 0 && a2 >= 0)
-            {
-                lb_result.Text = "";
-                lb_result.Text = Convert.ToString(result);
-                lb_calculator.Text = lb_calculator.Text + " = " + lb_result.Text + " ...--> ";
-            }
-            else
-                lb_result.Text = "first value or the second is empty!!! ";
+            lb_calculator.Text = lb_calculator.Text + lb_result.Text;
+
+            lb_result.Text = "";
+            lb_result.Text = Convert.ToString(result);
+            lb_calculator.Text = lb_calculator.Text + " = " + lb_result.Text ;
         }
 
         private void delete_click(object sender, EventArgs e)
         {
             lb_result.Text = "";
+            lb_calculator.Text = "";
         }
     }
 }
